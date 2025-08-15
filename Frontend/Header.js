dcf2fd2d3dfe17c459a227de782c01acb0c44e7a
Iris Ta <85328654+irista24@users.noreply.github.com>
2025-08-15 11:18:08 -0700

    Update Header.js

--- Frontend/Header.js
+++ Frontend/Header.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-// Define the styles for the header component
 const headerStyle = {
-  textAlign: 'center', // Center-aligns the text
-  color: '#6d8d24',    // PSU Green color
-  fontSize: '2rem',    // Font size for the header
-  marginBottom: '20px', // Spacing below the header
-  padding: '10px',     // Padding around the header text
+  textAlign: 'center', 
+  color: '#6d8d24',    
+  fontSize: '2rem',    
+  marginBottom: '20px', 
+  padding: '10px',     
 };
 
-// Header component
 const Header = () => {
   return (
     <header style={headerStyle}>
